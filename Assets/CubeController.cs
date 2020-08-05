709bd47955bd8646c490e8051e0b0120f3b530ba
Taku951 <17itagaki@gmail.com>
2020-08-05 18:14:26 +0900

    GoForwardUnityChanプロジェクト　Lesson7課題提出
    衝突時の音声追加

---
+++ Assets/CubeController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeController : MonoBehaviour {
+
+    // キューブの移動速度
+    private float speed = -12;
+
+    // 消滅位置
+    private float deadLine = -10;
+
+    // Use this for initialization
+    void Start () {
+        GetComponent<AudioSource>().Stop();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        // キューブを移動させる
+        transform.Translate(this.speed * Time.deltaTime, 0, 0);
+
+        // 画面外に出たら破棄する
+        if (transform.position.x < this.deadLine)
+        {
+            Destroy(gameObject);
+        }
+	}
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+
+        // ブロック＆地面に衝突した場合（追加）
+        if (collision.gameObject.name == "ground" || collision.gameObject.name == "CubePrefab(Clone)")
+        {
+            GetComponent<AudioSource>().Play();
+        }
+        // ユニティに当たった場合
+        else if (collision.gameObject.name == "UnityChan2D")
+        {
+            GetComponent<AudioSource>().Stop();
+        }
+    }
+}
